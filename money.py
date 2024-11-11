326574644b9e1b1cb60550e2d0caf6c3c5d0e23d
kdtTetz <xenosign@naver.com>
2024-11-12 02:51:31 +0900

    241112 문제 풀이 추가

---
+++ money.py
@@ -0,0 +1,17 @@
+def solution(price, money, count):
+    answer = -1
+
+    totalCharge = 0
+
+    for i in range(1, count + 1) :
+        totalCharge += price * i
+
+    print(totalCharge)
+
+    answer = 0 if money - totalCharge >= 0 else totalCharge - money
+
+    return answer
+
+ex1 = [3, 20, 4]
+
+print(solution(ex1[0], ex1[1], ex1[2]))
